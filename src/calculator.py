828ba5e9db5d7555ec45e42c2eeaa9c68d3607ec
ahmed-akram <ahmedakram3ai@gmail.com>
2026-08-27 19:24:03 +0300

    Add Subtract Method

---
+++ src/calculator.py
@@ -0,0 +1,21 @@
+def add(n1: float, n2: float) -> float:
+    """
+    Args:
+        n1: First Float Number
+        n2: First Float Number
+    
+    Returns:
+        The Summation of these 2 numbers (n1 + n2)
+    """
+    return n1 + n2
+
+def subtract(n1: float, n2: float) -> float:
+    """
+    Args:
+        n1: First Float Number
+        n2: First Float Number
+    
+    Returns:
+        The Subtraction of these 2 numbers (n1 - n2)
+    """
+    return n1 - n2
