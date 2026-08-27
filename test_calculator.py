aaba90626a0367adfd8237025413cd2208f40ef1
ahmed-akram <ahmedakram3ai@gmail.com>
2026-08-27 14:53:33 +0300

    Add Best-Practice PyTest Code Parameterize

--- test_calculator.py
+++ test_calculator.py
@@ -1,18 +1,14 @@
 from calculator import add
 import pytest
 
-@pytest.fixture
-def number1():
-    return 5
 
-
-@pytest.fixture
-def number2():
-    return 10
-
-
-def test_add(number1, number2):
-    assert number1 + number2 == add(number1, number2)
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+@pytest.mark.parametrize("number1, number2, expected", [
+    (2, 3, 5),
+    (-1, 1, 0),
+    (10, 1, 11),
+    (0, 0, 0),
+    (5, 10, 15),
+    (100, -50, 50),
+])
+def test_add(number1, number2, expected):
+    assert add(number1, number2) == expected
