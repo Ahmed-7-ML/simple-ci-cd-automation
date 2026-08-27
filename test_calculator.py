c483e7f1edd6c977e128cec805d2e15d1e4b6422
ahmed-akram <ahmedakram3ai@gmail.com>
2026-08-27 15:07:06 +0300

    Add Ruff linting to CI

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,6 @@
 from calculator import add
 import pytest
 
-
 @pytest.mark.parametrize("number1, number2, expected", [
     (2, 3, 5),
     (-1, 1, 0),
@@ -10,5 +9,6 @@ import pytest
     (5, 10, 15),
     (100, -50, 50),
 ])
+
 def test_add(number1, number2, expected):
     assert add(number1, number2) == expected
